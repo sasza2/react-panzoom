41a33248f1af35225eae832f482ca4f489b41d7c
sasza <sasza@onet.eu>
2019-11-30 16:48:57 +0100

    test positionClone

---
+++ src/helpers/positionClone.test.js
@@ -0,0 +1,8 @@
+import positionClone from './positionClone';
+
+test('positionClone', () => {
+  const positionRef = {
+    current: { x: 50, y: 100 },
+  };
+  expect(positionClone(positionRef)).toStrictEqual({ x: 50, y: 100 });
+});
