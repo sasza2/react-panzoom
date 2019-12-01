055fb214aa47b053cb301233bcc5ff0e4b408b6a
sasza <sasza@onet.eu>
2019-12-01 15:30:19 +0100

    test api setPosition

--- src/api/position.test.js
+++ src/api/position.test.js
@@ -1,6 +1,6 @@
 import { createRef } from 'react';
 
-import { getPosition } from './position';
+import { getPosition, setPosition } from './position';
 
 test('api/position/get', () => {
   const positionRef = createRef();
@@ -10,4 +10,29 @@ test('api/position/get', () => {
 
   positionRef.current = { x: 60, y: 25 };
   expect(getPosition({ positionRef })()).toStrictEqual({ x: 60, y: 25 });
+});
+
+test('api/position/set', () => {
+  const childRef = createRef();
+  childRef.current = { style: {} };
+
+  const positionRef = createRef();
+  positionRef.current = { x: 250, y: 400 };
+
+  const zoomRef = createRef();
+  zoomRef.current = 1.1;
+
+  // To (200, 300)
+  setPosition({ childRef, positionRef, zoomRef })(200, 300);
+
+  expect(positionRef.current).toStrictEqual({ x: 200, y: 300 });
+  expect(zoomRef.current).toBe(1.1);
+  expect(childRef.current.style.transform).toBe('translate(200px, 300px) scale(1.1)');
+
+  // To (600, 100)
+  setPosition({ childRef, positionRef, zoomRef })(600, 100);
+
+  expect(positionRef.current).toStrictEqual({ x: 600, y: 100 });
+  expect(zoomRef.current).toBe(1.1);
+  expect(childRef.current.style.transform).toBe('translate(600px, 100px) scale(1.1)');
 });
