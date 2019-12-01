d1fb7b11ed423922a06b013ea4449866a2c9e4a7
sasza <sasza@onet.eu>
2019-12-01 14:57:14 +0100

    test api setZoom

--- src/api/zoom.test.js
+++ src/api/zoom.test.js
@@ -1,6 +1,6 @@
 import { createRef } from 'react';
 
-import { getZoom } from './zoom';
+import { getZoom, setZoom } from './zoom';
 
 test('api/zoom/get', () => {
   const zoomRef = createRef();
@@ -10,4 +10,27 @@ test('api/zoom/get', () => {
 
   zoomRef.current = 0.5;
   expect(getZoom({ zoomRef })()).toBe(0.5);
+});
+
+test('api/zoom/set', () => {
+  const childRef = createRef();
+  childRef.current = { style: {} };
+
+  const positionRef = createRef();
+  const zoomRef = createRef();
+
+  // Change to 1.5
+  positionRef.current = { x: 15, y: 30 };
+  setZoom({ childRef, positionRef, zoomRef })(1.5);
+
+  expect(childRef.current.style.transform).toBe('translate(15px, 30px) scale(1.5)');
+  expect(positionRef.current).toStrictEqual({ x: 15, y: 30 });
+  expect(zoomRef.current).toBe(1.5);
+
+  // Change to 2.1
+  setZoom({ childRef, positionRef, zoomRef })(2.1);
+
+  expect(childRef.current.style.transform).toBe('translate(15px, 30px) scale(2.1)');
+  expect(positionRef.current).toStrictEqual({ x: 15, y: 30 });
+  expect(zoomRef.current).toBe(2.1);
 });
