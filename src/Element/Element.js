3ef5db0ea968a3f72568e364d01eb7970bdb3378
sasza <sasza@onet.eu>
2020-02-07 20:17:50 +0100

    Element mousemove

--- src/Element/Element.js
+++ src/Element/Element.js
@@ -1,18 +1,50 @@
-import React, { useLayoutEffect, useRef } from 'react';
+import React, {
+  useEffect, useLayoutEffect, useRef, useState,
+} from 'react';
 import PropTypes from 'prop-types';
+
+import positionFromEvent from '../helpers/positionFromEvent';
 
 import './Element.css';
 
 const Element = ({ children, x, y }) => {
+  const [moving, setMoving] = useState(null);
   const elementRef = useRef();
 
   useLayoutEffect(() => {
     elementRef.current.style.transform = `translate(${x}px, ${y}px)`;
   }, [x, y]);
 
+  useEffect(() => {
+    if (!moving) return undefined;
+
+    const mousemove = (e) => {
+      const eventPosition = positionFromEvent(e);
+      const translate = {
+        x: eventPosition.clientX - moving.x,
+        y: eventPosition.clientY - moving.y,
+      };
+
+      elementRef.current.style.transform = `translate(${translate.x}px, ${translate.y}px)`;
+    };
+
+    window.addEventListener('mousemove', mousemove);
+
+    return () => {
+      window.removeEventListener('mousemove', mousemove);
+    };
+  }, [moving]);
+
   const mousedown = (e) => {
     e.preventDefault();
     e.stopPropagation();
+
+    const eventPosition = positionFromEvent(e);
+    const rect = elementRef.current.getBoundingClientRect();
+    setMoving({
+      x: eventPosition.clientX - rect.left,
+      y: eventPosition.clientY - rect.top,
+    });
   };
 
   useLayoutEffect(() => {
