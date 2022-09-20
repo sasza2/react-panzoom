59b43bfb8cd413fc644d7448dccf7976b82617e3
sasza <sasza@onet.eu>
2022-09-20 16:44:06 +0200

    ts wip

---
+++ src/helpers/produceStyle.ts
@@ -0,0 +1,13 @@
+import { Position } from 'types'
+
+type TransformProps = {
+  position: Position,
+  zoom: number,
+}
+
+const transform = ({ position, zoom }: TransformProps): string => {
+  if (zoom) return `translate(${position.x}px, ${position.y}px) scale(${zoom})`;
+  return `translate(${position.x}px, ${position.y}px)`;
+};
+
+export default transform;
