ea41582d5b3b49f2e90ecbe4c2bf82d541f55b41
sasza <sasza@onet.eu>
2022-09-20 23:31:19 +0200

    ts wip

--- src/Select/context/SelectContext.js
+++
@@ -1,5 +0,0 @@
-import { createContext } from 'react';
-
-const SelectContext = createContext({});
-
-export default SelectContext;
--- src/Select/context/useSelect.js
+++
@@ -1,7 +0,0 @@
-import { useContext } from 'react';
-
-import SelectContext from './SelectContext';
-
-const useSelect = () => useContext(SelectContext);
-
-export default useSelect;
