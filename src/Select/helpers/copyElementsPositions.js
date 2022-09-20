425ac8f0dec86f3bae0f4f0fb4cc38d19b3de496
sasza <sasza@onet.eu>
2022-09-20 22:12:42 +0200

    ts wip

--- src/Select/helpers/copyElementsPositions.js
+++
@@ -1,9 +0,0 @@
-const copyElementsPositions = (elements) => {
-  const positions = {};
-  Object.entries(elements).forEach(([id, element]) => {
-    positions[id] = { ...element.position };
-  });
-  return positions;
-};
-
-export default copyElementsPositions;
