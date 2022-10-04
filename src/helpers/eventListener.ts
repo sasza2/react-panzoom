658a43707e9d9ac8fa587632aca71bf9548c42ed
sasza <sasza@onet.eu>
2022-10-04 23:38:14 +0200

    event listener changes

--- src/helpers/eventListener.ts
+++ src/helpers/eventListener.ts
@@ -1,4 +1,6 @@
-type EventNames = Array<'mousedown' | 'mouseup' | 'touchstart' | 'touchend' | 'mousemove' | 'touchmove'>
+import isEventMobileZoom from './isEventMobileZoom'
+
+type EventNames = Array<'mousedown' | 'mouseup' | 'touchstart' | 'touchend' | 'touchcancel' | 'mousemove' | 'touchmove'>
 
 type Callback = (e: MouseEvent) => void
 
@@ -18,12 +20,30 @@ const eventListener = (node: Window | HTMLDivElement, eventNames: EventNames, ca
 
 export const onMouseDown = (node: HTMLDivElement, callback: Callback): EventListenerClean => (
   eventListener(node, ['mousedown', 'touchstart'], callback)
-);
+)
 
-export const onMouseUp = (callback: Callback): EventListenerClean => (
-  eventListener(window, ['mouseup', 'touchend'], callback)
-);
+export const onMouseUp = (node: Window | HTMLDivElement, callback: Callback): EventListenerClean => {
+  const cleanMouseUp = eventListener(window, ['mouseup'], callback)
+  const cleanTouch = eventListener(node, ['touchend', 'touchcancel'], callback)
+  return () => {
+    cleanMouseUp()
+    cleanTouch()
+  }
+}
 
-export const onMouseMove = (callback: Callback): EventListenerClean => (
-  eventListener(window, ['mousemove', 'touchmove'], callback)
-);
+export const onMouseMove = (callback: Callback): EventListenerClean => {
+  const cleanMouseMove = eventListener(window, ['mousemove'], callback)
+
+  const callbackMobileWrapper: Callback = (e) => {
+    if (isEventMobileZoom(e)) return
+
+    callback(e)
+  }
+
+  const cleanTouchMove = eventListener(window, ['touchmove'], callbackMobileWrapper)
+
+  return () => {
+    cleanMouseMove()
+    cleanTouchMove()
+  }
+};
