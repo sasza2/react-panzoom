6621ab1ef611bcbcdc8b66b5f83cf71c08490bc0
sasza <sasza@onet.eu>
2022-11-30 00:20:51 +0100

    Fix for extra offset while panning on zoomout

--- src/helpers/appendToCurrentPosition.ts
+++ src/helpers/appendToCurrentPosition.ts
@@ -66,15 +66,18 @@ const appendToCurrentPosition = ({
     y: positionRef.current.y + toAdd.y,
   }
 
-  if (toAdd.x || toAdd.y) {
-    if (positionRef.current.x > 0) positionRef.current.x = 0
-    if (positionRef.current.y > 0) positionRef.current.y = 0
-
-    const diffX = distanceToRightEdge(childRef, positionRef)
-    if (diffX < 0) positionRef.current.x -= diffX
-
+  if (toAdd.y < 0) {
     const diffY = distanceToBottomEdge(childRef, positionRef)
     if (diffY < 0) positionRef.current.y -= diffY
+  } else if (toAdd.y > 0) {
+    if (positionRef.current.y > 0) positionRef.current.y = 0
+  }
+
+  if (toAdd.x < 0) {
+    const diffX = distanceToRightEdge(childRef, positionRef)
+    if (diffX < 0) positionRef.current.x -= diffX
+  } else if (toAdd.x > 0) {
+    if (positionRef.current.x > 0) positionRef.current.x = 0
   }
 
   childRef.current.style.transform = produceStyle({
